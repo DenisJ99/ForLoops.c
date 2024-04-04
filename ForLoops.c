b3e04f05bf8387ac732821e3ea2a3904c5abf1bd
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-04-04 14:54:06 +0100

    Add files via upload

---
+++ ForLoops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    for(int i =10; i >= 1; i -=3)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+
+}
+
+// For loop = repeats a section of code a limited amount of times
